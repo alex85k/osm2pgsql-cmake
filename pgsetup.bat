191600363f8aed1ede382a16fb86270a3f575cae
alex85k <alex85k@github.com>
2014-10-06 17:17:09 +0400

    try to setup PostgreSQL for testing

--- pgsetup.bat
+++ pgsetup.bat
@@ -1,7 +1,10 @@
 setlocal
 SET PATH="C:\Program Files\PostgreSQL\9.3\bin";%PATH%
-mkdir temp
 set PGUSER=postgres
 set PGPASSWORD=Password12!
+mkdir temp
+cacls temp /T /E /G Users:F
+cacls temp /T /E /G "Network Service":F
+cacls temp /T /E /G postgres:F
 psql -c "CREATE TABLESPACE tablespacetest LOCATION '%CD:\=/%/temp'"
 
